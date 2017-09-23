d396e09f34ab04213375dbc4b948d200da4c6373
8ctopotamus <joshnaylor88@gmail.com>
2017-09-23 11:17:07 -0500

    added menu

---
+++ Assets/Scripts/Menu/HelpPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPanel : MonoBehaviour {
+
+	void Start () {
+		gameObject.SetActive(false);
+	}
+
+	public void OnConfirm () {
+		gameObject.SetActive (false);
+	}
+
+}
